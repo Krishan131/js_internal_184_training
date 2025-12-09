28beb11439add59174febb6ef83beeb48f587b42
Krishan131 <krishan.kumarsingh0102@gmail.com>
2025-12-09 05:30:10 +0000

    day2

---
+++ Day2/investigateVars.js
@@ -0,0 +1,34 @@
+/*let a = 23
+const b = "Abc"
+var c = 78
+d = 88
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)*/
+//____________________________________________________________________
+
+//datatypes in js
+//boolean , numbers, bigInt , null , string , undefined
+
+//____________________________________________________________________
+
+/*let a = "abc"//string
+let b = ""//undefined
+let c = null//object(type) <= important
+let d;//undefined
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(typeof(d))*/
+
+//"use strict";
+
+let score = "33abcd"
+console.log(typeof(score))
+console.log(typeof score)
+
+let scan = Number(score)
+console.log(typeof scan)
+console.log(scan)
